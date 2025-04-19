0abf29c2401968d484a3235d31b72b838bcedd69
Ronaldo Mendes <mendes-ronaldo@live.com>
2025-04-19 18:35:32 -0300

    Using Embrapa API to download CSV files dynamically

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.auth.routes import auth_router
+from src.client.embrapa import embrapa_router
 
 app = FastAPI(
     version='1.0.0',
@@ -9,3 +10,4 @@ app = FastAPI(
 )
 
 app.include_router(auth_router, prefix='/v1/api/auth', tags=['Auth Controller'])
+app.include_router(embrapa_router, prefix='/v1/api', tags=['External Client Controller'])

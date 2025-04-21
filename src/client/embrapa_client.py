189c813541c927394e093b85ad88f26fee37b89c
Ronaldo Mendes <mendes-ronaldo@live.com>
2025-04-20 22:12:38 -0300

    Fixing param name to category

--- src/client/embrapa_client.py
+++ src/client/embrapa_client.py
@@ -14,12 +14,12 @@ class EmbrapaClient:
     """Class for processing Embrapa external communication"""
 
     async def process_data(self, client: AsyncClient, subcategory: str,
-                           param: EmbrapaParams) -> ViticultureCreateModel:
+                           category: EmbrapaParams) -> ViticultureCreateModel:
         """
         Process all requested data and returns a valid object for database persistence
         :param client: AsyncClient variable
         :param subcategory: selected subcategory in str format
-        :param param: enum EmbrapaParams
+        :param category: enum EmbrapaParams
         :return: ViticultureCreateModel response
         """
         response = await client.get(url=url.format(subcategory), timeout=10)
@@ -28,17 +28,17 @@ class EmbrapaClient:
             raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail=error_msg)
         response.encoding = 'utf-8'
 
-        result_model = await self.data_to_dict(param, response, subcategory)
+        result_model = await self.data_to_dict(category, response, subcategory)
         return ViticultureCreateModel(**result_model)
 
-    async def data_to_dict(self, param: EmbrapaParams, response: Response, subcategory: str) -> dict:
+    async def data_to_dict(self, category: EmbrapaParams, response: Response, subcategory: str) -> dict:
         """
         Convert received data in dict
-        :param param: enum EmbrapaParams
+        :param category: enum EmbrapaParams
         :param response: response from AsyncClient
         :param subcategory: variable in str format
         :return: dict result
         """
         df = pd.read_csv(StringIO(response.text), sep=';', encoding='utf8')
         result = df.to_json(orient='records', lines=False)
-        return {'category': param.name, 'subcategory': subcategory, 'data': result}
+        return {'category': category.name, 'subcategory': subcategory, 'data': result}

b8a0749396e736a936be5887395daff69bd56a50
Ronaldo Mendes <mendes-ronaldo@live.com>
2025-04-20 21:04:31 -0300

    Splitting internal and external communication

--- src/client/routes.py
+++ src/client/routes.py
@@ -1,7 +1,4 @@
-from io import StringIO
-
 import httpx
-import pandas as pd
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -9,14 +6,15 @@ from starlette.responses import JSONResponse
 
 from src.auth.dependencies import AccessTokenBearer
 from src.client.enums import EmbrapaParams
-from src.client.schemas import ViticultureCreateModel
+from src.client.embrapa_client import EmbrapaClient
 from src.client.services import ViticultureService
-from src.client.utils import menus, url
+from src.client.utils import menus
 from src.db.main import get_session
 
 client_router = APIRouter()
 access_token_bearer = AccessTokenBearer()
 viticulture_service = ViticultureService()
+feign_client = EmbrapaClient()
 
 
 @client_router.get('/embrapa/external_content/{param}')
@@ -27,24 +25,15 @@ async def get_data_from_embrapa_by_param(param: EmbrapaParams,
     API responsible for download all CSV files from Embrapa website based
     on each category param and save in database
     """
-    async with httpx.AsyncClient() as client:
-        exists = await viticulture_service.data_exists(menus[param.name], session)
-        if exists:
-            raise HTTPException(status_code=status.HTTP_208_ALREADY_REPORTED,
-                                detail='All data already exists in database.')
+    exists = await viticulture_service.data_exists(menus[param.name], session)
+    if exists:
+        raise HTTPException(status_code=status.HTTP_208_ALREADY_REPORTED,
+                            detail='All data already exists in database.')
 
+    async with httpx.AsyncClient() as client:
         try:
             for option in menus[param.name]:
-                response = await client.get(url=url.format(option), timeout=10)
-                if response.status_code != 200:
-                    error_msg = 'An error occurred during external request. Try again later!'
-                    raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail=error_msg)
-                response.encoding = 'utf-8'
-                df = pd.read_csv(StringIO(response.text), sep=';', encoding='utf8')
-                result = df.to_json(orient='records', lines=False)
-
-                result_model = {'category': param.name, 'subcategory': option, 'data': result}
-                viticulture = ViticultureCreateModel(**result_model)
+                viticulture = await feign_client.process_data(client, option, param)
                 await viticulture_service.create_data(viticulture, session)
             return JSONResponse(content={'message': 'All data saved successfully in database'})
         except Exception as e:

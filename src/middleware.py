44a2b406c79f6f15eacd814662f3f37580582187
Ronaldo Mendes <mendes-ronaldo@live.com>
2025-04-21 22:15:15 -0300

    Fixing headers

--- src/middleware.py
+++ src/middleware.py
@@ -28,7 +28,8 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(CORSMiddleware,
                        allow_origins=['*'],
-                       allow_methods=['GET', 'POST', 'PATCH', 'DELETE', 'OPTION', 'PUT'],
+                       allow_methods=['*'],
                        allow_headers=['*'],
                        allow_credentials=True)
-    app.add_middleware(TrustedHostMiddleware, allowed_hosts=['localhost', '127.0.0.1', Config.DOMAIN_URL])
+    app.add_middleware(TrustedHostMiddleware,
+                       allowed_hosts=['localhost', '127.0.0.1', Config.DOMAIN_URL])
